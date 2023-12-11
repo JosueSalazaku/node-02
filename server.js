3c1813db5c7ebb4c7a5d54c145c9460dedb0bd10
Josué Salazaku <josuesalazaku@gmail.com>
2023-12-11 18:59:27 +0100

    improve file path

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const path = require("path");
 const fs = require("fs");
-const os = require("node:os");
-
-console.log(path.__dirnam);
+const os = require("os");
 
 function createFolder(folderPath, callback) {
   fs.mkdir(folderPath, (err) => {
@@ -34,12 +32,11 @@ const clientPath = "client";
 
 createFolder(clientPath, () => {
   const newFilePath = path.join(clientPath, "index.html");
-  createFile(newFilePath, "Hello, this is HTML content!");
   const htmlContent = `<!DOCTYPE html>
     <html>
       <head>
         <title>My Dynamic HTML Page</title>
-        <link rel="stylesheet" href="/client/style.css">
+        <link rel="stylesheet" href="style.css">
       </head>
       <body>
         <h1>CLIENT PAGE</h1>
@@ -49,7 +46,6 @@ createFolder(clientPath, () => {
   createFile(newFilePath, htmlContent);
 
   const cssFile = path.join(clientPath, "style.css");
-  createFile(cssFile, "Hello, this is CSS content!");
   const cssContent = `body {
     font-family: "Poppins", sans-serif;
     background: turquoise;
@@ -61,105 +57,103 @@ createFolder(clientPath, () => {
     width: 100vw;
     height: 100vh;
   }`;
+
   createFile(cssFile, cssContent);
 
   const contactFolderPath = path.join(clientPath, "contact");
   createFolder(contactFolderPath, () => {
     const contactHtml = path.join(contactFolderPath, "index.html");
-    createFile(contactHtml, "Hello, this is HTML content for contact!");
-    const htmlContent = `<!DOCTYPE html>
-    <html>
-      <head>
-        <title>My Dynamic HTML Page</title>
-        <link rel="stylesheet" href="/client/contact/style.css">
-      </head>
-      <body>
-        <h1>CONTACT PAGE</h1>
-      </body>
-    </html>`;
+    const contactHtmlContent = `<!DOCTYPE html>
+      <html>
+        <head>
+          <title>My Dynamic HTML Page</title>
+          <link rel="stylesheet" href="../style.css">
+        </head>
+        <body>
+          <h1>CONTACT PAGE</h1>
+        </body>
+      </html>`;
 
-    createFile(contactHtml, htmlContent);
+    createFile(contactHtml, contactHtmlContent);
 
-    const contactCss = path.join(contactFolderPath, "style.css");
-    createFile(contactCss, "Hello, this is CSS content for contact!");
-    const cssContent = `body {
-        font-family: "Poppins", sans-serif;
-        background: yellow;
-        color: blueviolet;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width: 100vw;
-        height: 100vh;
-      }`;
-    createFile(contactCss, cssContent);
+    const contactCssFile = path.join(contactFolderPath, "style.css");
+    const contactCssContent = `body {
+      font-family: "Poppins", sans-serif;
+      background: yellow;
+      color: blueviolet;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      width: 100vw;
+      height: 100vh;
+    }`;
+
+    createFile(contactCssFile, contactCssContent);
   });
 
   const aboutFolderPath = path.join(clientPath, "about");
   createFolder(aboutFolderPath, () => {
     const aboutHtml = path.join(aboutFolderPath, "index.html");
-    createFile(aboutHtml, "Hello, this is HTML content for about!");
-    const htmlContent = `<!DOCTYPE html>
+    const aboutHtmlContent = `<!DOCTYPE html>
       <html>
         <head>
           <title>My Dynamic HTML Page</title>
-          <link rel="stylesheet" href="/client/about/style.css">
+          <link rel="stylesheet" href="../style.css">
         </head>
         <body>
           <h1>ABOUT PAGE</h1>
         </body>
       </html>`;
 
-    createFile(aboutHtml, htmlContent);
+    createFile(aboutHtml, aboutHtmlContent);
 
-    const aboutCss = path.join(aboutFolderPath, "style.css");
-    createFile(aboutCss, "Hello, this is CSS content for about!");
-    const cssContent = `body {
-        font-family: "Poppins", sans-serif;
-        background: blue;
-        color: blueviolet;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width: 100vw;
-        height: 100vh;
-      }`;
-    createFile(aboutCss, cssContent);
+    const aboutCssFile = path.join(aboutFolderPath, "style.css");
+    const aboutCssContent = `body {
+      font-family: "Poppins", sans-serif;
+      background: blue;
+      color: blueviolet;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      width: 100vw;
+      height: 100vh;
+    }`;
+
+    createFile(aboutCssFile, aboutCssContent);
   });
 
   const blogFolderPath = path.join(clientPath, "blog");
   createFolder(blogFolderPath, () => {
     const blogHtml = path.join(blogFolderPath, "index.html");
-    createFile(blogHtml, "Hello, this is HTML content for blog!");
-    const htmlContent = `<!DOCTYPE html>
+    const blogHtmlContent = `<!DOCTYPE html>
       <html>
         <head>
           <title>My Dynamic HTML Page</title>
-          <link rel="stylesheet" href="/client/blog/style.css">
+          <link rel="stylesheet" href="../style.css">
         </head>
         <body>
           <h1>BLOG PAGE</h1>
         </body>
       </html>`;
 
-    createFile(blogHtml, htmlContent);
+    createFile(blogHtml, blogHtmlContent);
 
-    const blogCss = path.join(blogFolderPath, "style.css");
-    createFile(blogCss, "Hello, this is CSS content for blog!");
-    const cssContent = `body {
-        font-family: "Poppins", sans-serif;
-        background: purple;
-        color: blueviolet;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width: 100vw;
-        height: 100vh;
-      }`;
-    createFile(blogCss, cssContent);
+    const blogCssFile = path.join(blogFolderPath, "style.css");
+    const blogCssContent = `body {
+      font-family: "Poppins", sans-serif;
+      background: green;
+      color: blueviolet;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      width: 100vw;
+      height: 100vh;
+    }`;
+
+    createFile(blogCssFile, blogCssContent);
   });
 
   const clientTxtfile = path.join(clientPath, "info.txt");
