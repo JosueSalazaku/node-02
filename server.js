d477108a208f027a53799359c3ea022ada8cb9d7
Josué Salazaku <josuesalazaku@gmail.com>
2023-12-11 11:02:05 +0100

    Put file path in to a variable

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const fs = require("fs");
 console.log("Hello world, this is Node.js");
 console.log(path.basename(__filename));
 
-fs.readFile(
-  "/Users/josuesalazaku/Developer/BeCode/TheHill/back-End/node-02/server.js",
-  "utf8",
-  (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  }
-);
+let myPath =
+  "/Users/josuesalazaku/Developer/BeCode/TheHill/back-End/node-02/server.js";
+
+fs.readFile(myPath, "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
+// fs.mkdirSync(
+//   "/Users/josuesalazaku/Developer/BeCode/TheHill/back-End/node-02/server.js"
+// );
