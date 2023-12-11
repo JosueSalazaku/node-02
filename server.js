35095138c846179dcc025141220708b38c9e01f7
Josué Salazaku <josuesalazaku@gmail.com>
2023-12-11 10:57:22 +0100

    Read server.js by adding utf8

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ console.log(path.basename(__filename));
 
 fs.readFile(
   "/Users/josuesalazaku/Developer/BeCode/TheHill/back-End/node-02/server.js",
+  "utf8",
   (err, data) => {
     if (err) throw err;
-    console.log(data.toString());
+    console.log(data);
   }
 );
